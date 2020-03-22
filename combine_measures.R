5cf2c4cef444792ed6c88118cef8e216fcb4c66e
Simone Baffelli <simone.baffelli@gmail.com>
2020-03-22 08:18:22 +0100

    Update data paths.

--- combine_measures.R
+++ combine_measures.R
@@ -3,8 +3,14 @@ checkpoint("2020-03-03")
 library(tidyverse)
 library(sf)
 
-
-files_to_join <- Sys.glob("measures*.csv")
+#Find all measures
+files_to_join <- Sys.glob("data/measures*.csv")
+#Read them in
 all_measures <- purrr::map(files_to_join, readr::read_csv)
+#Bind them together
+combined_measures <- bind_rows(all_measures)
+#Save them
+readr::write_csv(combined_measures, "data/combined_measures.csv")
 
-bind_rows(all_measures)
+#Also save them in wide format
+
