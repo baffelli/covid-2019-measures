0d932baedd0f85f8a9ff2758090e2f8eefe9f2fd
Simone Baffelli <simone.baffelli@gmail.com>
2020-03-24 11:44:53 +0100

    Saving one file per measure for the cantons which don't have measures.

--- combine_measures.R
+++ combine_measures.R
@@ -2,13 +2,27 @@ library(checkpoint)
 checkpoint("2020-03-03")
 library(tidyverse)
 library(sf)
-
+library(googledrive)
 #Find all measures
-files_to_join <- Sys.glob("data/measures*.csv")
+files_to_join <- append(Sys.glob("data/measures_[A-Z]*.csv"), "data/measures.csv")
+
+
+#Set column types
+col_types <- cols(
+  date_implemented = col_datetime(),
+  date_lifted = col_datetime(),
+  measure = col_double(),
+  level = col_character(),
+  unit = col_character(),
+  source = col_character()
+)
+
+
+
 #Read them in
-all_measures <- purrr::map(files_to_join, readr::read_csv)
+all_measures <- purrr::map(files_to_join, purrr::safely(~readr::read_csv(.x, col_types = col_types)))
 #Bind them together
-combined_measures <- bind_rows(all_measures)
+combined_measures <- bind_rows(all_measures %>% map("result"))
 #Save them
 readr::write_csv(combined_measures, "data/combined_measures.csv", na = "")
 
@@ -17,4 +31,6 @@ combined_measures %>%
   group_by(nesting=unit) %>% 
   mutate(measure_cat=NA) %>%
   nest() %>% 
-  mutate(out = map2(data, nesting, ~write_csv(.x, path =  stringr::str_interp("data/measures_to_write_${.y}.csv"))))
+  mutate(
+    out = map2(data, nesting, ~write_csv(.x, path =  stringr::str_interp("data/measures_to_edit_${.y}.csv"))))
+  
