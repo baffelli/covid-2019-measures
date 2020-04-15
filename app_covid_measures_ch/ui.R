50b3d1d74742a6fc3bee9ae7b76af60313fb1925
Johannes Bracher <you@example.com>
2020-04-15 13:42:28 +0200

    Minor change in app

--- app_covid_measures_ch/ui.R
+++ app_covid_measures_ch/ui.R
@@ -18,11 +18,14 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   # Show a plot of the generated distribution
   mainPanel(
+    hr(),
     tags$div(class="header", checked=NA,
              tags$p("This Shiny app provides an interactive display of the data set on COVID19 intervention measures in Switzerland provided and described",
-                    tags$a(href="https://github.com/baffelli/covid-2019-measures", "here"),
-                    "To see the details (incl. a link to the respective source) for a specific canton and day click into the plot.")
+                    tags$a(href = "https://github.com/baffelli/covid-2019-measures", "here"),
+                    "To see the details (incl. a link to the respective source) for a specific canton and day click into the plot."),
+             tags$p("This is work and progress as we will be update and complete the data over the coming weeks.")
     ),
+    hr(),
     selectInput("category", label = "Choose category of intervantion:",
                 choices = c("Recommendations and information campaigns" = "recommendations",
                             "Events and mass gatherings" = "events",
@@ -39,7 +42,19 @@ shinyUI(fluidPage(
                             "Day care" = "daycare"),
                 selected = "events"),
     plotOutput("distPlot", width = "150%", height = "600px", click = "coord_click", hover = "coord_hover"),
-    h2("Details (upon click on dots in plot):"),
-    htmlOutput("details")
+    hr(),
+    tags$b("Details (upon click on dots in plot):"),
+    hr(),
+    htmlOutput("details"),
+    hr(),
+    tags$p(""),
+    tags$p("Data collection: Simone Baffelli, Fabienne Krauer, Johannes Bracher,
+           Maria Bekker-Nielsen Dunbar, Muriel Buri, Nicolo Lardelli, Jonas Oesch."),
+    tags$p("Figures: Fabienne Krauer",
+           tags$a(href = "https://github.com/fkrauer",
+                  "https://github.com/fkrauer")),
+    tags$p("Shiny app: Johannes Bracher",
+           tags$a(href = "https://github.com/jbracher",
+                  "https://github.com/jbracher"))
   )
 ))
